8c4d80faeb94457267f3d345c1b01c63b4d02327
Charlie DeTar <cfd@media.mit.edu>
2012-10-28 20:29:01 -0400

    Updating for hurricane sandy

--- get_livestreams_and_names.py
+++ get_livestreams_and_names.py
@@ -1,12 +1,11 @@
 import os
+import csv
 import re
 import json
+import StringIO
 import codecs
 import requests
 import urllib
-from BeautifulSoup import BeautifulSoup
-
-root = "http://occupystreams.org"
 
 def get_cached_page(url):
     name = os.path.join(
@@ -26,88 +25,31 @@ def get_cached_page(url):
         content = fh.read()
     return content
 
-soup = BeautifulSoup(get_cached_page(root))
-links = soup.findAll("a")
-ids = set()
+reader = csv.reader(open("Hurricane Sandy livestreams - Sheet1.csv"))
 sources = []
-for link in links:
-    if link['href'].startswith("/item/occupy"):
-        subpage = "".join((root, link['href']))
-        subsoup = BeautifulSoup(get_cached_page(subpage))
-        iframes = subsoup.findAll("iframe")
-        if not iframes:
-            continue
-        where = link.text
-        where = where.replace("Occupy ", "")
-        where = where.replace(" - UStream", "")
-        where = where.replace(" - Livestream", "")
-        iframe = iframes[0]
-        # Livestream
-        match = re.match(r'^.*livestream.com/embed/([^\?]*)(\?.*)?$', iframe['src'])
-        provider = None
-        if match:
-            item = match.group(1)
-            provider = "livestream"
-
-        # Ustream
-        match = re.match(r'^.*ustream.tv/(?:embed|socialstream)/(.*)$', iframe['src'])
-        if match:
-            item = match.group(1)
-            provider = "ustream"
-
-        # justin.tv
-        match = re.match(r'^.*justin\.tv/chat/embed\?channel=([^\&]*)\&.*$', iframe['src'])
-        if match:
-            item = match.group(1)
-            provider = "justintv"
-
-        assert provider is not None, "Source not recognized: '%s'" % iframe['src']
-
-        unique = ":".join((provider, item))
-        if unique not in ids:
-            sources.append({
-                'provider': provider,
-                'id': item,
-                'location': where,
-                'url': "".join((root, link['href'])),
-            })
-        ids.add(unique)
-
-for source in sources:
-    # Special case non-geocodable locations
-    locname = source['location']
-    if locname.startswith("Wall Sreet"): # [sic]
-        locname = "Wall Street, NY"
-    elif "London" in locname:
-        locname = "London"
-    elif "Denver" in locname:
-        locname = "Denver"
-    elif "Phoenix" in locname:
-        locname = "Phoenix, AZ"
-    elif "Los Angeles" in locname:
-        locname = "Los Angeles"
-    elif "Boston" in locname:
-        locname = "Boston"
-    elif "Harvard" in locname:
-        locname = "Harvard, Cambridge, MA"
-    elif "Gorleben" in locname:
-        locname = "Gorleben, Germany"
-    elif "Fort Meyers" in locname:
-        locname = "Fort Meyers, Florida"
-    elif "San Diego" in locname:
-        locname = "San Diego, CA"
-    elif locname.startswith("Wall Street"):
-        locname = "Wall Street, NY"
-    elif locname == "LA - OLA Newscast":
-        locname = "Los Angeles, CA"
-    elif locname == "LA - Civic Engagement":
-        locname = "Los Angeles, CA"
-
+items = iter(reader)
+items.next()
+for url,locname in items:
+    source = {
+            'location': locname,
+            'url': url,
+    }
+    if "ustream" in url:
+        source['provider'] = "ustream"
+    elif "livestream" in url:
+        source['provider'] = "livestream"
+        source['id'] = re.search("livestream.com/([^/]+)/", url).group(1)
+    elif "justin" in url:
+        source['provider'] = "justintv"
+        source['id'] = re.search("justin.tv/([^/]+)(/|$)", url).group(1)
+    else:
+        assert False, "Unknown provider %s" % url
 
     latlng = json.loads(get_cached_page(
         "https://maps.googleapis.com/maps/api/geocode/json?sensor=false&address=%s" % urllib.quote_plus(locname.encode('utf8'))
     ))
     assert latlng['status'] != 'ZERO_RESULTS', "Geocode not found for %s" % locname
     source['point'] = latlng['results'][0]['geometry']['location']
+    sources.append(source)
 
 print "var data = %s" % json.dumps({'sources': sources})
